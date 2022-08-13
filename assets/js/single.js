4c4ab90ed75b04a7776ad0a252717073ae98d58a
mitchgm <mitchgm@bgsu.edu>
2022-08-13 16:07:21 -0700

    added link to re direct to github to view more than 30 repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,6 +10,11 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
       // pass response data to dom function
       displayIssues(data);
+
+      // check if api has paginated issues
+      if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
        
     });
   }
@@ -52,13 +59,27 @@ var getRepoIssues = function(repo) {
 
     // append to container
     issueEl.appendChild(typeEl);
+
+    // append to the dom
     issueContainerEl.appendChild(issueEl);
     }
 
   };
 
 
+  var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
+    // creating link
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 
 
