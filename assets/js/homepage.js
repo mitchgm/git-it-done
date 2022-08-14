8a3319e4895449b8bee2695a7d38809bec2f921f
mitchgm <mitchgm@bgsu.edu>
2022-08-13 18:08:10 -0700

    added the ability to search repo by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
     var getUserRepos = function(user) {
         // format the github api url
@@ -31,10 +32,33 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 
         if (username) {
         getUserRepos(username); // this matches the username to the github account and passes the data
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
         } else {
         alert("Please enter a GitHub username");
         }
+      };
+
+      var getFeaturedRepos = function(language) {
+        var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+      
+        fetch(apiUrl).then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        }
+            else {
+                alert('Error: GitHub User Not Found');
+            }
+        })
+            .catch(function(error) {
+                alert("Unable to connect to GitHub");
+              });
+            
+      
+        
+        
       };
 
       var displayRepos = function(repos, searchTerm) {
@@ -85,6 +109,20 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
         }
       }
 
+      var  buttonClickHandler = function(event) {
+            var language = event.target.getAttribute("data-language");
+            console.log(language);
+            if (language) {
+                getFeaturedRepos(language);
+              
+                // clear old content
+                repoContainerEl.textContent = "";
+              }
+      };
+
+
+
+    languageButtonsEl.addEventListener("click", buttonClickHandler);
       userFormEl.addEventListener("submit", formSubmitHandler);
 
 
